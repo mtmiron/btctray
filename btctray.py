f2ec0a07eb7d4e0d1facb993b5f80b50784ea9bf
Murray <Murray.T.Miron@gmail.com>
2014-06-14 19:43:39 -0400

    Add a timestamp to the displayed price + reset autoupdate countdown when user manually updates via clicking.

--- btctray.py
+++ btctray.py
@@ -2,14 +2,17 @@
 
 
 import os
+import sys
 import gtk
 import gio
 import urllib
 import json
 import glib
+import time
 
 
 class BTCTray(gtk.StatusIcon):
+        # The spot_rate is defined by Coinbase as falling somewhere between their current buy & sell prices
         PRICEURL = 'https://coinbase.com/api/v1/prices/spot_rate'
         UPDATEINTERVAL = 300
 
@@ -32,8 +35,8 @@ class BTCTray(gtk.StatusIcon):
 			('About', gtk.STOCK_ABOUT, '_About...', None, 'About BTCTray', self.on_about),
                 ]
 
-                self.sets = gtk.settings_get_default()
-                self.sets.set_long_property('gtk-tooltip-timeout', 0, 'BTCTray.__init__')
+                sets = gtk.settings_get_default()
+                sets.set_long_property('gtk-tooltip-timeout', 0, 'BTCTray.__init__')
 
 		ag = gtk.ActionGroup('Actions')
 		ag.add_actions(actions)
@@ -41,42 +44,62 @@ class BTCTray(gtk.StatusIcon):
 		self.manager.insert_action_group(ag, 0)
 		self.manager.add_ui_from_string(menu)
 		self.menu = self.manager.get_widget('/Menubar/Menu/About').props.parent
+
                 self.set_from_file("bitcoin-black.png")
 		self.set_visible(True)
 		self.connect('activate', self.on_activate)
 		self.connect('popup-menu', self.on_popup_menu)
+                self.price = { 'amount': '0.00', 'currency': 'USD' }
 
-	def on_activate(self, data):
+
+        def on_activate(self, data):
+            # Reset our update timer since user has just forced a refresh
+            glib.source_remove(self.current_timeout_id)
+            self.current_timeout_id = glib.timeout_add_seconds(BTCTray.UPDATEINTERVAL, self.update_price)
             self.update_price()
+
 
 	def on_popup_menu(self, status, button, time):
 		self.menu.popup(None, None, None, button, time)
+
 
         def on_about(self, data):
 		dialog = gtk.AboutDialog()
 		dialog.set_name('BTCTray')
                 dialog.set_authors(('Murray Miron',))
                 dialog.set_website('https://github.com/mtmiron/btctray')
-		dialog.set_comments('A bitcoin price "widget."  Click to issue a manual price update (default interval ' + str(BTCTray.UPDATEINTERVAL) + ' secs).')
+		dialog.set_comments('A bitcoin price "widget."  Click to manually update price (auto updates every ' + str(BTCTray.UPDATEINTERVAL) + ' secs).')
 		dialog.run()
 		dialog.destroy()
+
 
         def on_quit(self, data):
             os._exit(0)
 
-        def update_price(self):
+
+        def update_price(self, data = None, uri = PRICEURL):
+            '''Updates the current price via a REST call.'''
             try:
-                url = urllib.urlopen(BTCTray.PRICEURL)
+                url = urllib.urlopen(uri)
                 resp = url.read()
                 url.close()
                 self.price = json.loads(resp)
-		self.set_tooltip(self.price['amount'] + " " + self.price['currency'])
+                str_price = "%s %s" % (self.price['amount'], self.price['currency'])
+		self.set_tooltip(str_price + ' [%s]' % (time.strftime("%X"),))
             except IOError as err:
-                price = self.get_tooltip_text()
-                self.set_tooltip(str(err) + " (last known price " + str(price) + ")")
+                str_price = "%s %s" % (self.price['amount'], self.price['currency'])
+                self.set_tooltip(str(err) + " (last known price " + str_price + ')')
             return True
 
+
         def daemonize(self):
+            try:
+                sys.stdin.close()
+                sys.stdout.close()
+                sys.stderr.close()
+            except (OSError, IOError, SystemError):
+                pass
+
             try:
                 pid = os.fork()
             except OSError, e:
@@ -99,5 +122,5 @@ if __name__ == '__main__':
 	app = BTCTray()
         app.daemonize()
         app.update_price()
-        glib.timeout_add_seconds(BTCTray.UPDATEINTERVAL, app.update_price)
+        app.current_timeout_id = glib.timeout_add_seconds(BTCTray.UPDATEINTERVAL, app.update_price)
 	gtk.mainloop()
