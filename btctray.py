491c9ffabea8b4b97be995ae9448285b23381143
Murray <Murray.T.Miron@gmail.com>
2014-06-14 16:03:10 -0400

    Added icon file + finished implementing features.

--- btctray.py
+++ btctray.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 
-# price URL: https://coinbase.com/api/v1/prices/spot_rate
-
-
 
 import os
 import gtk
+import gio
+import urllib
+import json
+import glib
 
-class TrackerStatusIcon(gtk.StatusIcon):
+
+class BTCTray(gtk.StatusIcon):
+        PRICEURL = 'https://coinbase.com/api/v1/prices/spot_rate'
+        UPDATEINTERVAL = 300
+
 	def __init__(self):
 		gtk.StatusIcon.__init__(self)
 		menu = '''
@@ -26,35 +31,32 @@ class TrackerStatusIcon(gtk.StatusIcon):
                         ('Quit', gtk.STOCK_QUIT, '_Quit', None, 'Quit the application', self.on_quit),
 			('About', gtk.STOCK_ABOUT, '_About...', None, 'About BTCTray', self.on_about),
                 ]
+
 		ag = gtk.ActionGroup('Actions')
 		ag.add_actions(actions)
 		self.manager = gtk.UIManager()
 		self.manager.insert_action_group(ag, 0)
 		self.manager.add_ui_from_string(menu)
 		self.menu = self.manager.get_widget('/Menubar/Menu/About').props.parent
-		#search = self.manager.get_widget('/Menubar/Menu/Search')
-		#search.get_children()[0].set_markup('<b>_Search...</b>')
-		#search.get_children()[0].set_use_underline(True)
-		#search.get_children()[0].set_use_markup(True)
-		#search.get_children()[1].set_from_stock(gtk.STOCK_FIND, gtk.ICON_SIZE_MENU)
-		self.set_from_stock(gtk.STOCK_FIND)
-		self.set_tooltip('Tracker Desktop Search')
+
+                self.set_from_file("bitcoin-black.png")
 		self.set_visible(True)
+
 		self.connect('activate', self.on_activate)
 		self.connect('popup-menu', self.on_popup_menu)
 
 	def on_activate(self, data):
-		os.spawnlpe(os.P_NOWAIT, 'tracker-search-tool', os.environ)
+            self.update_price()
+            #os.spawnlpe(os.P_NOWAIT, 'tracker-search-tool', os.environ)
 
 	def on_popup_menu(self, status, button, time):
 		self.menu.popup(None, None, None, button, time)
 
         def on_about(self, data):
 		dialog = gtk.AboutDialog()
-		dialog.set_name('Tracker')
-		dialog.set_version('0.5.0')
-		dialog.set_comments('A desktop indexing and search tool')
-		dialog.set_website('www.freedesktop.org/Tracker')
+		dialog.set_name('BTCTray')
+		dialog.set_version('0.1')
+		dialog.set_comments('A bitcoin price "widget."  Click to issue a manual price update (default interval ' + str(BTCTray.UPDATEINTERVAL) + 's).')
 		dialog.run()
 		dialog.destroy()
 
@@ -62,8 +64,21 @@ class TrackerStatusIcon(gtk.StatusIcon):
             exit(0)
 
 
+        def update_price(self):
+            try:
+                url = urllib.urlopen(BTCTray.PRICEURL)
+                resp = url.read()
+                url.close()
+                self.price = json.loads(resp)
+		self.set_tooltip(self.price['amount'] + " " + self.price['currency'])
+            except IOError:
+                pass
+
+
+
 if __name__ == '__main__':
-	TrackerStatusIcon()
+	app = BTCTray()
+        app.update_price()
+
+        glib.timeout_add_seconds(BTCTray.UPDATEINTERVAL, app.update_price)
 	gtk.main()
-
-
